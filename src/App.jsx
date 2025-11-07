5cf74e92239183280974d7b9eac8c187803a910f
asifalam515 <asibulalam515@gmail.com>
2025-11-07 18:07:32 +0600

    square button created

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Square from "./Square";
+
+const App = () => {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">Tic Tac Toe</h1>
+      <Square></Square>
+    </div>
+  );
+};
+
+export default App;
